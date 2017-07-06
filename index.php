034fcea48127db9981b1daea070ce1ffca8951e3
Vall <michal.deluga@milenium.it>
2017-07-06 17:35:24 +0200

    FIX: analytics? FIX: orbitron font to https

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
   <title>Canonn | Thargoid Structures and UL Destinations</title>
-  <link href="http://fonts.googleapis.com/css?family=Orbitron:400" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Orbitron:400" rel="stylesheet" type="text/css" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&amp;subset=latin-ext" rel="stylesheet">
   <link href="us_sites_mapping/css/us_map.css" rel="stylesheet" type="text/css" />
   <!-- Custom CSS -->
@@ -48,6 +48,15 @@
     font-size: 0.8rem;
   }*/
   </style>
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-102148507-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 
 </head>
 
@@ -199,15 +208,6 @@
 
   });
   
-  </script>
-
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-102148507-1', 'auto');
-    ga('send', 'pageview');
   </script>
 
 </body>
